4e5aff4c18d7f1cbcf7ff8c57f554b9af6f32282
max <maxo@stanford.edu>
2018-08-31 08:55:23 -0700

    Removed graphhpper core files.
    The OSM files got too big so this repo will just be the code to interact with graphhopper.

--- graphhopper/run.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-REM changing the directory
-cd C:\Users\Derek\Documents\CS_work\graphhopper
-REM opening the browser - has to open first because graphhopper runs until exited
-start chrome http://localhost:8989/
-REM starting graphhopper
-java -jar graphhopper-web-0.5.0-with-dep.jar jetty.resourcebase=webapp config=config-example.properties osmreader.osm=berlin-latest.osm.pbf
